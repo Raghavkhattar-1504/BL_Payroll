0c7fe0beb902aed6618810f9cc630db365fde8ea
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-01-28 01:42:26 +0530

    [Raghav] : REFACTOR - Updated Json server

--- JS/payrollform.js
+++ JS/payrollform.js
@@ -1,7 +1,31 @@
 const form = document.querySelector("form");
-console.log("iinside js")
+// console.log("iinside js")
+
+document.addEventListener("DOMContentLoaded", function () {
+    const fetchData = JSON.parse(localStorage.getItem("userData"));
+    if (fetchData) {
+        document.querySelector('#namebox').value = fetchData.name;
+        document.querySelector('#notes').value = fetchData.notes;
+        document.querySelector(`input[name='profile-image'][value='${fetchData.pi}']`).checked = true; 
+        document.querySelector(`input[name='genbox'][value = '${fetchData.gen}']`).checked = true;
+        fetchData.dpt.forEach(element => {
+            document.querySelector(`input[name='dpt'][value = '${element}']`).checked = true;
+        });
+        document.querySelector('#salary').value = fetchData.sal;
+        document.querySelector('#date').value = fetchData.startdate.date;
+        document.querySelector('#month').value = fetchData.startdate.month;
+        document.querySelector('#year').value = fetchData.startdate.year;
+    }
+    else{
+        // alert("No employee found!");
+    }
+})
+
 form.addEventListener("submit" , function (e) {
     
+    
+
+
     e.preventDefault()
     const name = document.querySelector("#namebox").value;
     const notes = document.querySelector("#notes").value;
@@ -28,15 +52,43 @@ form.addEventListener("submit" , function (e) {
     }
 
     console.log("userdata",obj)
+    const dataChk = JSON.parse(localStorage.getItem('userData'));
+    if(!dataChk){
+    fetch('http://localhost:3000/emplist', {
+        method : 'POST',
+        headers : {
+            'Content-type' : 'application/json',
+        },
+        body : JSON.stringify(obj), 
+    })
+    .then((res) => {
+        if(!res.ok){
+            throw new Error(`${res.status}`);
+        }
+        return res.json();
+    })
+    .then((data) => {
+        console.log('Success', data);
+    })
+    .catch((error) => {
+        console.log('Error' , error);
+    })}
 
-    if(localStorage.getItem('userData')){
-        const data = JSON.parse(localStorage.getItem('userData'))
-        data.push(obj)
-        localStorage.setItem('userData',JSON.stringify(data));
-    }else{
-        localStorage.setItem("userData",JSON.stringify([obj]))
+    else{
+
+
+        fetch(`http://localhost:3000/emplist/${dataChk.id}` , {
+            method : 'PUT',
+            body :    JSON.stringify(obj)
+        })
+        .then(() => console.log("Employee data updated."))
+        .catch((err) => console.log(`Error : ${err}`))
+
+        localStorage.removeItem('userData');
+
     }
-    
-    alert('Employee added successfullly!')
+
+
+
     window.location.href = 'payrollhome.html'
 })
--- JS/payrollhome.js
+++ JS/payrollhome.js
@@ -3,57 +3,86 @@ console.log("userDara", userdata)
 
 $(document).ready(() => {
 
-    if (userdata) {
-        userdata.forEach((user, ind) => {
-            const dept = user.dpt
-                .map((dept) => `<span class= "badge">${(dept)}</span>`).join("").slice(" ");
+    $.ajax({
+        url: 'http://localhost:3000/emplist',
+        type: 'GET',
+        success: function (userdata) {
+            if (userdata) {
+                userdata.forEach((user, ind) => {
+                    const dept = user.dpt
+                        .map((dept) => `<span class= "badge">${(dept)}</span>`).join("").slice(" ");
 
-            let datestr = ''
+                    let datestr = ''
 
-            console.log(user.startdate)
+                    console.log(user.startdate)
 
-            if (user.startdate.date === "1" && user.startdate.month === '1' && user.startdate.year === '1') {
-                datestr = `01 January 1965`;
+                    if (user.startdate.date === "1" && user.startdate.month === '1' && user.startdate.year === '1') {
+                        datestr = `01 January 1965`;
+                    }
+                    else if (user.startdate.date === "2" && user.startdate.month === '2' && user.startdate.year === '2') {
+                        datestr = `11 May 1986`;
+                    }
+                    else if (user.startdate.date === "3" && user.startdate.month === '3' && user.startdate.year === '3') {
+                        datestr = `21 October 2006`;
+                    }
+
+                    const userId = user.id;
+                    
+                    const row = `
+                    <tr>
+                                    <td><img src= ${user.pi} alt="Avatar"> ${user.name}</td>
+                                    <td>${user.gen}</td>
+                                    <td>
+                                        ${dept}
+                                    </td>
+                                    <td>${user.sal}</td>
+                                    <td>${datestr}</td>
+                                    <td class="actions">
+                                        <img onclick= "editUser('${userId}')" src="../Assets/pencil.png" alt="Edit" class="edit-logo">
+                                        </img>
+                                        <img onclick= "deleteUser('${userId}')" src="../Assets/bin.png" alt="Delete" class="delete-logo">
+                                        </img>
+                                    </td>
+                                </tr>`
+
+                    $(".emp-bdy-tbl-tbody").append(row);
+                });
             }
-            else if (user.startdate.date === "2" && user.startdate.month === '2' && user.startdate.year === '2') {
-                datestr = `11 May 1986`;
-            }
-            else if (user.startdate.date === "3" && user.startdate.month === '3' && user.startdate.year === '3') {
-                datestr = `21 October 2006`;
-            }
-          
-            const row = `
-            <tr>
-                            <td><img src= ${user.pi} alt="Avatar"> ${user.name}</td>
-                            <td>${user.gen}</td>
-                            <td>
-                                ${dept}
-                            </td>
-                            <td>${user.sal}</td>
-                            <td>${datestr}</td>
-                            <td class="actions">
-                                <img onclick= "editUser(${ind})" src="../Assets/pencil.png" alt="Edit" class="edit-logo">
-                                </img>
-                                <img onclick= "deleteUser(${ind})" src="../Assets/bin.png" alt="Delete" class="delete-logo">
-                                </img>
-                            </td>
-                        </tr>`
+        }
+    })
 
-            $(".emp-bdy-tbl-tbody").append(row);
-        });
-    }
 });
 
 function deleteUser(index) {
-    userdata.splice(index, 1);
-    localStorage.setItem('userData' , JSON.stringify(userdata));
-    alert("Employee Deleted");
-    window.location.reload();
+    console.log(index);
+    
+    $.ajax({
+        url : `http://localhost:3000/emplist/${index}`,
+        method : 'DELETE' , 
+        success : function (res) {
+            console.log('Employee deleted successfully: ', res);
+        },
+        error: function (error) {
+            console.log(`Error occured : ${error}`);
+        },
+     })
 }
 
+
 function editUser(index) {
-    console.log(index);
-    console.log("Edit icon clicked");
-    window.location.href = `payrollform.html?index = ${index}`;
+    // console.log("in");
+    
+$.ajax({
+    url : `http://localhost:3000/emplist/${index}`,
+    method: 'GET',
+    success : (function (res) {
+        localStorage.setItem('userData' , JSON.stringify(res));
+        window.location.href = "payrollform.html"
+    })
+    
+})
+
+
+
 }
 
